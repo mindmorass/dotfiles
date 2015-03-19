35a9dd470847bd492869a56dc89d7603c034fab5
mindmorass <mindmorass@gmail.com>
2015-03-19 14:52:11 -0700

    importing .bashrc.d

---
+++ .bashrc.d/02-aliases.sh
@@ -0,0 +1,37 @@
+source $HOME/.functions/sysinfo/platform.lib
+
+os_x_aliases() {
+    alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+    alias wifi="/usr/sbin/system_profiler SPAirPortDataType"
+    alias serial="/usr/sbin/system_profiler SPHardwareDataType | grep 'Serial Number'"
+}
+
+linux_aliases() {
+    # TODO
+    echo ""
+}
+
+global_aliases() {
+    alias ll='ls -l'
+    alias la='ls -A'
+    alias lla='ls -lA'
+    alias l='ls -CF'
+    alias g='git'
+    alias cget='curl -C - -O'
+}
+
+case "${MY_PLATFORM}" in
+    darwin)
+        global_aliases
+        os_x_aliases
+        ;;
+    inux)
+        global_aliases
+        linux_aliases
+        ;;
+    *)
+        global_aliases
+        ;;
+esac
+
+
---
+++ .bashrc.d/99-ovftool.sh
@@ -0,0 +1,5 @@
+#TODO - fix $HOME/.functions/path.lib to take paths with spaces
+
+if [ -d "/Applications/VMware OVF Tool" ]; then
+    export PATH=$PATH:/Applications/VMware\ OVF\ Tool
+fi
