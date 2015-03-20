2848379a56b8f3618d279bae7f8acd31c137673f
mindmorass <mindmorass@gmail.com>
2015-03-19 20:15:23 -0700

    better function name for lscolors

--- .profile.d/01-lscolors.sh
+++ .profile.d/01-lscolors.sh
@@ -1,17 +1,17 @@
 source $HOME/.functions/profile.lib
 source $HOME/.functions/sysinfo/platform.lib
 
-profile_aliases() {
+set_ls_colors() {
     local plugin_dir=$(_plugin_dir)
     local my_plugin=$(profile_plug "sigurdga/ls-colors-solarized")
 
     if test -e $(which dircolors); then
-        eval $(dircolors -b ${plugin_dir}/${my_plugin}/dircolors)
+        eval $(dircolors ${plugin_dir}/${my_plugin}/dircolors)
     fi
     
-    if [ "${SYS_PLATFORM}" == "darwin" ]; then
+    if [[ "${SYS_PLATFORM}" == "darwin" ]]; then
         export CLICOLOR=1
     fi
 }
 
-profile_aliases
+set_ls_colors
