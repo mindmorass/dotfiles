bcd07844d5a7f4a9cfa1f48b450118fbb6f2dbfc
mindmorass <mindmorass@gmail.com>
2015-03-29 15:32:00 -0700

    fix for PS1 colors - new set of variables

--- .profile.d/03-prompt.sh
+++ .profile.d/03-prompt.sh
@@ -1,5 +1,3 @@
-#https://github.com/git/git/tree/master/contrib/completion
-
 source $HOME/.functions/profile.lib
 source $HOME/.functions/shell.lib
 source $HOME/.functions/colors.lib
@@ -11,27 +9,24 @@ scripts_dir="${profile_d}/scripts"
 
 source $scripts_dir/git/git-prompt.sh
 
-shell=$(which_shell)
-case $shell in
-    bash)
-        # git completion sourced from 
-        # https://github.com/git/git/tree/master/contrib/completion
-        # consider git archive in the plugin modules
-        source $scripts_dir/git/git-completion.bash
-        # drop a newline between prompts
-        PROMPT_COMMAND="echo"
-        PS1="${yellowf}${boldoff}(${cyanf}\u${whitef}${boldoff}@${cyanf}\h${redf}${boldon} \W\$(__git_ps1 ' ${greenf}[${purplef}%s${greenf}]${reset}')${reset}${yellowf})${reset}\n${blackf}${boldon}|${whitef}${boldoff}[${whitef}${boldon} \d - \T ${whitef}${boldoff}]${blackf}${boldon}|${reset}\n ${redf}${boldon}\\_${yellowf}${boldoff} \$${reset} "
-        ;;
-    zsh)
-        source $scripts_dir/git/git-completion.zsh
-        setopt PROMPT_SUBST ; PS1='[%n@%m %c$(__git_ps1 " (%s)")]\$ '
-        ;;
-esac
+set_prompt() {
+    local shell=$(which_shell)
 
-# hack ... 
-# be nice and set colors back to normal for scripts
-# should find a way to make the color scheming more generic
-# test something like this
-# https://linuxtidbits.wordpress.com/2008/08/11/output-color-on-bash-scripts/
+    case $shell in
+        bash)
+            # git completion sourced from 
+            # https://github.com/git/git/tree/master/contrib/completion
+            # consider git archive in the plugin modules
+            source $scripts_dir/git/git-completion.bash
+            # drop a newline between prompts
+            PROMPT_COMMAND="echo"
+            PS1="${ps_yellowf}${ps_boldoff}(${ps_cyanf}\u${ps_whitef}${ps_boldoff}@${ps_cyanf}\h${ps_redf}${ps_boldon} \W\$(__git_ps1 ' ${ps_greenf}[${ps_purplef}%s${ps_greenf}]${ps_reset}')${ps_reset}${ps_yellowf})${ps_reset}\n${ps_blackf}${ps_boldon}|${ps_whitef}${ps_boldoff}[${ps_whitef}${ps_boldon} \d - \T ${ps_whitef}${ps_boldoff}]${ps_blackf}${ps_boldon}|${ps_reset}\n ${ps_redf}${ps_boldon}\\_${ps_yellowf}${ps_boldoff} \$${ps_reset} "
+            ;;
+        zsh)
+            source $scripts_dir/git/git-completion.zsh
+            setopt PROMPT_SUBST ; PS1='[%n@%m %c$(__git_ps1 " (%s)")]\$ '
+            ;;
+    esac
+}
 
-_colors_initialize_ansi
+set_prompt
